937f205017fb1b24767bb4ef4fa55290561c9481
S3Prototype <112780106+benjaminaugust@users.noreply.github.com>
2022-11-26 07:38:16 -0600

    Clean up comments

--- cli/build/src/utils/convert/convertChapters.js
+++ cli/build/src/utils/convert/convertChapters.js
@@ -15,75 +15,19 @@ exports.default = (manuscriptPath, verbose = false) => {
 const createChapters = (directoryList, verbose = false) => {
     if (verbose)
         (0, printWhite_1.default)(`Converting chapters...`);
-    // let currentSection: any;
-    // directoryList.forEach((filePath) => {
-    //   //Make an object to represent the file
-    //   const fileData = {
-    //     filePath,
-    //     isSection: !filePath.includes(".md"),
-    //     files: [],
-    //   };
-    //   //If the file is a section and we have no section
-    //   //already defined, now that's the section we'll
-    //   //start pushing stuff into. Return.
-    //   if (fileData.isSection && !currentSection) {
-    //     currentSection = fileData;
-    //     return;
-    //   }
-    //   printWhite(`\nReading from filepath ${filePath}`);
-    //   if (currentSection) {
-    //     //If the file we're analyzing isn't a section,
-    //     //and we already have a current section,
-    //     //then check if the file should be in the section.
-    //     //If so, push it into the current section and return.
-    //     if (filePath.includes(currentSection.filePath))
-    //       return currentSection.files.push(filePath);
-    //     //File is not a section, or it's a new section,
-    //     //or it's a file that doesn't belong to any section.
-    //     //So push the current section. We're done with it
-    //     chapterArray.push(currentSection);
-    //   }
-    //   //If it's a section, set current section to that. return
-    //   if (fileData.isSection) {
-    //     currentSection = fileData;
-    //     return;
-    //   }
-    //   //It's not a section, and doesn't
-    //   //belong to a section, so just push it
-    //   chapterArray.push(fileData);
-    //   //No current section exists. We just pushed
-    //   //a plain file
-    //   currentSection = null;
-    // });
-    // printBlue(JSON.stringify(chapterArray, null, 2));
     const segments = directoryList.map((item) => ({
         path: item,
         splits: item.split("\\"),
     }));
-    // console.log(segments);
     const orderedSegments = segments
         .map((item) => {
         const { splits } = item;
-        // for (let i = 0; i < splits.length - 1; i++) {
         const currSplit = splits[0];
         const justTheNumber = currSplit.replace("~ ", "");
         const order = parseInt(justTheNumber);
-        // }
         return Object.assign(Object.assign({}, item), { order });
     })
         .sort((0, sort_by_1.default)("order"));
-    // const orderedGroups: any = [];
-    // const orderList = orderedSegments.map((segment) => segment.order);
-    // for (let i = 1; i < orderedSegments.length; i++) {
-    //   const start = orderList.indexOf(i);
-    //   const end = orderList.lastIndexOf(i);
-    //   // printBlue(orderedSegments[start].splits[0]);
-    //   orderedGroups.push({
-    //     // topPath:
-    //     topOrder: i,
-    //     group: orderedSegments.slice(start, end),
-    //   });
-    // }
     const reOrder = (segments) => {
         const orderList = segments.map((segment) => segment.order);
         const highestOrder = Math.max(...orderList);
@@ -94,15 +38,10 @@ const createChapters = (directoryList, verbose = false) => {
                 continue;
             const end = endIndex + 1;
             const setToOrder = segments.slice(start, end);
-            // console.log(`i: ${i}, start: ${start}, end: ${end}`);
-            // if (i == 17) console.log("Set to order", setToOrder);
             const x = setToOrder
                 .map((item) => {
                 const currSplit = item.splits[1];
-                // if (j > 1) printBlue(`\nCurrsplit: ${currSplit}`);
                 const justTheNumber = currSplit.split("~ ")[0];
-                // if (i == 17)
-                //   console.log(`split: ${currSplit}, number: ${justTheNumber}`);
                 const order = parseInt(justTheNumber);
                 return Object.assign(Object.assign({}, item), { order: order });
             })
@@ -111,14 +50,10 @@ const createChapters = (directoryList, verbose = false) => {
             const segment1 = segments.slice(0, start);
             const segment2 = segments.slice(end);
             segments = [...segment1, ...x, ...segment2];
-            // if (i == 17) console.log("Set after reorder", x);
-            // if (i == 17) console.log("New segments", segments);
         }
         return segments;
     };
     const finalSegments = reOrder(orderedSegments);
-    // console.log(orderedSegments);
-    // console.log(JSON.stringify(finalSegments, null, 2));
     const sortedDirectoryList = finalSegments.map((segment) => segment.path);
     const chapterArray = [];
     sortedDirectoryList.forEach((item) => {
@@ -128,14 +63,11 @@ const createChapters = (directoryList, verbose = false) => {
             const splits = segment.split("~ ");
             if (splits.length < 1)
                 throw new Error(`Invalid file or folder name: "${segment}"`);
-            // console.log(chalk.cyan(splits));
             const result = splits.filter((_, i) => i > 0).join();
-            // console.log(chalk.cyan(result));
             const formattedSegment = {
                 rawName: result,
                 order: parseInt(splits[0]),
             };
-            // console.log(chalk.cyan(JSON.stringify(formattedSegment, null, 2)));
             return formattedSegment;
         })
             .forEach(({ rawName, order }) => {
@@ -146,15 +78,11 @@ const createChapters = (directoryList, verbose = false) => {
                     isSection: !rawName.includes(".md"),
                     order,
                 };
-                // printBlue(rawName);
                 chapterArray.push(sectionData);
             }
         });
     });
     if (verbose)
         (0, printWhite_1.default)(`Got the following chapters: ${JSON.stringify(chapterArray, null, 2)}`);
-    // console.log("ChapterArray", JSON.stringify(chapterArray, null, 2));
-    //Cycle through
-    //Check every item against each section
     return chapterArray;
 };
--- cli/src/utils/convert/convertChapters.ts
+++ cli/src/utils/convert/convertChapters.ts
@@ -21,89 +21,23 @@ export default (manuscriptPath: string, verbose = false): any[] => {
 const createChapters = (directoryList: string[], verbose = false): any[] => {
   if (verbose) printWhite(`Converting chapters...`);
 
-  // let currentSection: any;
-  // directoryList.forEach((filePath) => {
-  //   //Make an object to represent the file
-  //   const fileData = {
-  //     filePath,
-  //     isSection: !filePath.includes(".md"),
-  //     files: [],
-  //   };
-
-  //   //If the file is a section and we have no section
-  //   //already defined, now that's the section we'll
-  //   //start pushing stuff into. Return.
-  //   if (fileData.isSection && !currentSection) {
-  //     currentSection = fileData;
-  //     return;
-  //   }
-
-  //   printWhite(`\nReading from filepath ${filePath}`);
-
-  //   if (currentSection) {
-  //     //If the file we're analyzing isn't a section,
-  //     //and we already have a current section,
-  //     //then check if the file should be in the section.
-  //     //If so, push it into the current section and return.
-  //     if (filePath.includes(currentSection.filePath))
-  //       return currentSection.files.push(filePath);
-
-  //     //File is not a section, or it's a new section,
-  //     //or it's a file that doesn't belong to any section.
-  //     //So push the current section. We're done with it
-  //     chapterArray.push(currentSection);
-  //   }
-
-  //   //If it's a section, set current section to that. return
-  //   if (fileData.isSection) {
-  //     currentSection = fileData;
-  //     return;
-  //   }
-
-  //   //It's not a section, and doesn't
-  //   //belong to a section, so just push it
-  //   chapterArray.push(fileData);
-  //   //No current section exists. We just pushed
-  //   //a plain file
-  //   currentSection = null;
-  // });
-
-  // printBlue(JSON.stringify(chapterArray, null, 2));
-
   const segments = directoryList.map((item) => ({
     path: item,
     splits: item.split("\\"),
   }));
 
-  // console.log(segments);
-
   const orderedSegments: any[] = segments
     .map((item) => {
       const { splits } = item;
-      // for (let i = 0; i < splits.length - 1; i++) {
       const currSplit = splits[0];
       const justTheNumber = currSplit.replace("~ ", "");
       const order = parseInt(justTheNumber);
-      // }
       return {
         ...item,
         order,
       };
     })
     .sort(sortBy("order"));
-
-  // const orderedGroups: any = [];
-  // const orderList = orderedSegments.map((segment) => segment.order);
-  // for (let i = 1; i < orderedSegments.length; i++) {
-  //   const start = orderList.indexOf(i);
-  //   const end = orderList.lastIndexOf(i);
-  //   // printBlue(orderedSegments[start].splits[0]);
-  //   orderedGroups.push({
-  //     // topPath:
-  //     topOrder: i,
-  //     group: orderedSegments.slice(start, end),
-  //   });
-  // }
 
   const reOrder = (segments: any) => {
     const orderList = segments.map((segment: any) => segment.order);
@@ -116,15 +50,10 @@ const createChapters = (directoryList: string[], verbose = false): any[] => {
 
       const end = endIndex + 1;
       const setToOrder = segments.slice(start, end);
-      // console.log(`i: ${i}, start: ${start}, end: ${end}`);
-      // if (i == 17) console.log("Set to order", setToOrder);
       const x = setToOrder
         .map((item: any) => {
           const currSplit = item.splits[1];
-          // if (j > 1) printBlue(`\nCurrsplit: ${currSplit}`);
           const justTheNumber = currSplit.split("~ ")[0];
-          // if (i == 17)
-          //   console.log(`split: ${currSplit}, number: ${justTheNumber}`);
           const order = parseInt(justTheNumber);
           return {
             ...item,
@@ -136,16 +65,11 @@ const createChapters = (directoryList: string[], verbose = false): any[] => {
       const segment1 = segments.slice(0, start);
       const segment2 = segments.slice(end);
       segments = [...segment1, ...x, ...segment2];
-      // if (i == 17) console.log("Set after reorder", x);
-      // if (i == 17) console.log("New segments", segments);
     }
     return segments;
   };
 
   const finalSegments = reOrder(orderedSegments);
-
-  // console.log(orderedSegments);
-  // console.log(JSON.stringify(finalSegments, null, 2));
 
   const sortedDirectoryList = finalSegments.map((segment: any) => segment.path);
 
@@ -158,14 +82,11 @@ const createChapters = (directoryList: string[], verbose = false): any[] => {
         const splits = segment.split("~ ");
         if (splits.length < 1)
           throw new Error(`Invalid file or folder name: "${segment}"`);
-        // console.log(chalk.cyan(splits));
         const result = splits.filter((_: any, i: number) => i > 0).join();
-        // console.log(chalk.cyan(result));
         const formattedSegment = {
           rawName: result,
           order: parseInt(splits[0]),
         };
-        // console.log(chalk.cyan(JSON.stringify(formattedSegment, null, 2)));
         return formattedSegment;
       })
       .forEach(({ rawName, order }: any) => {
@@ -176,7 +97,6 @@ const createChapters = (directoryList: string[], verbose = false): any[] => {
             isSection: !rawName.includes(".md"),
             order,
           };
-          // printBlue(rawName);
           chapterArray.push(sectionData);
         }
       });
@@ -186,10 +106,5 @@ const createChapters = (directoryList: string[], verbose = false): any[] => {
     printWhite(
       `Got the following chapters: ${JSON.stringify(chapterArray, null, 2)}`
     );
-
-  // console.log("ChapterArray", JSON.stringify(chapterArray, null, 2));
-
-  //Cycle through
-  //Check every item against each section
   return chapterArray;
 };
